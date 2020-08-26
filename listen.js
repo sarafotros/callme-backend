c19ffa8338be99c47a389ec30602bc92d35a710e
Sara Fotros <sarafotros@gmail.com>
2020-08-26 17:03:21 +0100

    deploy 3

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
+const app = require("./server")
 const PORT = process.env.PORT || 9090;
 
 // const { PORT = 9090 } = process.env;
-const app = require("./server")
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
