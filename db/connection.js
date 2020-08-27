9b06d9de8ea4b67518502d3f1a140d37180ef8d9
Sara Fotros <sarafotros@gmail.com>
2020-08-27 15:05:50 +0100

    deployment 2

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 require ('dotenv').config()
 const mongoose = require('mongoose')
+const mongoDB = process.env.MONGODB_URL
 const user = process.env.DB_USER
 const pass = process.env.DB_PASS
 const dbName = process.env.DB_NAME
 const dbCluster = process.env.DB_CLUSTER
 
-const mongoDB = `mongodb+srv://${user}:${pass}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+// const mongoDB = `mongodb+srv://${user}:${pass}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
